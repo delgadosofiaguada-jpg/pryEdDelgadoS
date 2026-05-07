8317f91e741b8eb5881ee47efe4f18ff5db1af38
delgadosofiaguada-jpg <delgado.sofiaguada@gmail.com>
2026-05-07 10:12:17 -0300

    CAMBIOS

--- pryEdDelgadoS/clsListaDoble.cs
+++ pryEdDelgadoS/clsListaDoble.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace pryEdDelgadoS
 {
@@ -61,5 +62,65 @@ namespace pryEdDelgadoS
                 }
             }
         }
+        public void Recorrer(DataGridView Grilla)
+        {
+            clsNodo aux = Primero;
+            Grilla.Rows.Clear();
+            while (aux != null)
+            {
+                Grilla.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+        public void RecorrerDes(DataGridView Grilla)
+        {
+            clsNodo aux = Ultimo;
+            Grilla.Rows.Clear();
+            while (aux != null)
+            {
+                Grilla.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                aux = aux.Anterior;
+            }
+        }
+        public void Eliminar(Int32 Codigo)
+        {
+            if (Primero.Codigo==Codigo && Ultimo == Primero)
+            {
+                Primero = null;
+                Ultimo = null;
+            }
+            else
+            {
+                 if (Primero.Codigo == Codigo)
+                 {
+                        Primero = Primero.Siguiente;
+                        Primero.Anterior = null;
+                 }
+                 else
+                 {
+                        if (Ultimo.Codigo == Codigo)
+                        {
+                            Ultimo = Ultimo.Anterior;
+                            Ultimo.Siguiente = null;
+                        }
+                        else
+                        {
+                            clsNodo aux = Primero;
+                            clsNodo ant = Primero;
+                            while (aux.Codigo < Codigo)
+                            {
+                                ant = aux;
+                                aux = aux.Siguiente;
+                            }
+                            aux= aux.Siguiente;
+                            ant.Siguiente = aux;
+                            aux.Anterior = ant;
+                        }
+                 }
+            }
+
+            
+        }
     }
 }
+
